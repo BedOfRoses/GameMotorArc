f4270ee7e68cd6af995e83b108a1b67e2314837a
BedOfRoses <50531780+BedOfRoses@users.noreply.github.com>
2023-10-02 01:54:11 +0200

    flipper

--- Assets/Flipper.cs
+++ Assets/Flipper.cs
@@ -6,24 +6,59 @@ using UnityEngine;
 public class Flipper : MonoBehaviour
 {
     
-    public HingeJoint hinge;
-    public Rigidbody cringe;
-    
+   [SerializeField] public HingeJoint leftFlipper;
+   [SerializeField] public HingeJoint rightFlipper;
+   
+   [SerializeField] public float leftTargetPos;
+   [SerializeField] public float leftRestPos = 0;
+   [SerializeField] public float RightTargetPos;
+   [SerializeField] public float rightRestPos = 0;
+
+   [SerializeField] private float maxPos;
+   [SerializeField] private float startPos;
+   [SerializeField] private float flipStrength;
+   [SerializeField] private float dampStrength;
+   
+
+    private void Start()
+    {
+        leftFlipper.useSpring = true;
+        rightFlipper.useSpring = true;
+    }
+
     private void FixedUpdate()
     {
+        var leftFlipperSpring = leftFlipper.spring;
+        leftFlipperSpring.spring = flipStrength;
+        
+        
+        var rightFlipperSpring = rightFlipper.spring;
+        rightFlipperSpring.spring = flipStrength;
+        
+        
+        leftFlipperSpring.damper = dampStrength;
+        rightFlipperSpring.damper = dampStrength;
+
         
         if (Input.GetKey(KeyCode.A))
         {
-            // swing oppover for hinge1
-            hinge.connectedBody.freezeRotation = true;
-            
+            //Left
+            leftFlipperSpring.targetPosition = leftTargetPos;
+            leftFlipper.spring = leftFlipperSpring;
+            rightFlipper.useLimits = true;
+
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             // swing oppover for hinge2
+            rightFlipperSpring.targetPosition = RightTargetPos;
+            rightFlipper.spring = rightFlipperSpring;
+            rightFlipper.useLimits = true;
         }
-        
-        
+
+        leftFlipperSpring.targetPosition = leftRestPos;
+        rightFlipperSpring.targetPosition = rightRestPos;
+
     }
 }
