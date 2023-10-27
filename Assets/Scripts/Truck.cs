b6d1d1995063e1a976702d91bf5f176c8f087c25
BedOfRoses <50531780+BedOfRoses@users.noreply.github.com>
2023-10-27 11:45:50 +0200

    Update Truck.cs

--- Assets/Scripts/Truck.cs
+++ Assets/Scripts/Truck.cs
@@ -7,21 +7,48 @@ public class Truck : MonoBehaviour
 {
 
     [SerializeField] private GameObject[] wheels;
-
     [SerializeField] private float k; // spring-constant
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private Vector3 F; // sum of Forces or more commonly known as Force
+    [SerializeField] private Vector3 W; // work.
+    [SerializeField] private float distance;
+    [SerializeField] private float length;
 
-    [SerializeField] private Rigidbody rb; 
-
-
-    void test()
+    private void Awake()
     {
-        Vector3 newforce = new Vector3();
-        rb.AddForce(newforce,ForceMode.Acceleration);
-        
+       
     }
+    
     
     private void FixedUpdate()
     {
-        throw new NotImplementedException();
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, length))
+        {
+            
+        }
+        
     }
+    
+    
+    public Vector3 SpringWork()
+    {
+        
+        return new Vector3();
+    }
+    
+    void test()
+    {
+        
+        Vector3 force1 = new Vector3(1, 1, 1);
+        Vector3 force2 = new Vector3(2, 2, 2);
+        Vector3 sum = force1 + force2;
+        Debug.Log("Sum of force is: " + sum.ToString("F2"));
+        
+        Vector3 newforce = new Vector3();
+        rb.AddForce(newforce,ForceMode.Acceleration);
+    }
+    
+    
+    
 }
