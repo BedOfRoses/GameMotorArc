8c6647e5cc5a21e59f35189ccb5f9b391191f91a
BedOfRoses <50531780+BedOfRoses@users.noreply.github.com>
2023-11-09 15:47:03 +0100

    Testing springjoint

--- Assets/Scripts/Truck.cs
+++ Assets/Scripts/Truck.cs
@@ -14,35 +14,93 @@ public class Truck : MonoBehaviour
     [SerializeField] private float distance;
     [SerializeField] private float length;
 
+
+    private void SetUpSpringComp()
+    {
+        foreach (var wheel in wheels)
+        {
+            // Add the spring component
+            wheel.AddComponent<SpringJoint>();
+
+            // Set the connectedBody to the Car's body
+            wheel.GetComponent<SpringJoint>().connectedBody = this.transform.gameObject.GetComponent<Rigidbody>();
+            wheel.GetComponent<SpringJoint>().damper = 0.2f;
+            wheel.GetComponent<SpringJoint>().maxDistance = 0f;
+            wheel.GetComponent<SpringJoint>().minDistance = 0f;
+            wheel.GetComponent<SpringJoint>().breakForce = Single.PositiveInfinity;
+            wheel.GetComponent<SpringJoint>().breakTorque = Single.PositiveInfinity;
+            
+            wheel.GetComponent<SpringJoint>().autoConfigureConnectedAnchor = true;
+            wheel.GetComponent<SpringJoint>().enableCollision = true;
+            wheel.GetComponent<SpringJoint>().spring = 10;
+            wheel.GetComponent<SpringJoint>().enablePreprocessing = true;
+
+
+            wheel.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX;
+            wheel.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionZ;
+
+
+        }
+        
+        
+    }
+    
     private void Awake()
     {
+        
+        SetUpSpringComp();
+        
        
+    }
+
+
+    void MoveFunction()
+    {
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            // add force left
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            // add force right
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            // add force forward
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            // add force backwards
+        }
+        
+        
+        
+        
     }
     
     
     
     private void FixedUpdate()
     {
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, length))
-        {
-            
-        }
+        //TODO: IMPLEMENT LATER
+       // RaycastHit hit;
+       // if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, length))
+       // {
+       //     
+       // }
+       
+       
+       
         
     }
     
     
-    void test()
-    {
-        
-        Vector3 force1 = new Vector3(1, 1, 1);
-        Vector3 force2 = new Vector3(2, 2, 2);
-        Vector3 sum = force1 + force2;
-        Debug.Log("Sum of force is: " + sum.ToString("F2"));
-        
-        Vector3 newforce = new Vector3();
-        rb.AddForce(newforce,ForceMode.Acceleration);
-    }
+   
     
     
     
