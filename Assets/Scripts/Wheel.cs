c56073fc5937f49187be2c729abe42346c30ffd6
BedOfRoses <50531780+BedOfRoses@users.noreply.github.com>
2023-11-09 16:38:48 +0100

    Wheel

---
+++ Assets/Scripts/Wheel.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheel : MonoBehaviour
+{
+
+    [SerializeField] private Rigidbody rb;
+
+
+    private void FixedUpdate()
+    {
+        //
+        //
+        //
+    }
+}
