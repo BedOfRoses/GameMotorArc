08e38ace2e9c838798a78a73c3ee9e95acf3a33c
BedOfRoses <50531780+BedOfRoses@users.noreply.github.com>
2023-11-10 12:15:12 +0100

    s

--- Assets/Scripts/TruckV2.cs
+++ Assets/Scripts/TruckV2.cs
@@ -14,7 +14,16 @@ public class TruckV2 : MonoBehaviour
     [SerializeField] private WheelCollider backLeftWheelCollider;
     [SerializeField] private WheelCollider backRightWheelCollider;
     #endregion
-   
+
+    #region WheelTransforms
+    [SerializeField] private Transform frontRightWheelTransform;
+    [SerializeField] private Transform frontLeftWheelTransform;
+    [SerializeField] private Transform backLeftWheelTransform;
+    [SerializeField] private Transform backRightWheelTransform;
+    #endregion
+
+    [SerializeField] private List<Transform> _WheelTransforms;
+    
     [SerializeField] private List<WheelCollider> _WheelColliders;
 
     [SerializeField] private double accel =  800.300;
@@ -36,6 +45,12 @@ public class TruckV2 : MonoBehaviour
         _WheelColliders.Add(frontRightWheelCollider);
         _WheelColliders.Add(backLeftWheelCollider);
         _WheelColliders.Add(backRightWheelCollider);
+        
+        _WheelTransforms.Add(frontLeftWheelTransform);
+        _WheelTransforms.Add(frontRightWheelTransform);
+        _WheelTransforms.Add(backLeftWheelTransform);
+        _WheelTransforms.Add(backRightWheelTransform);
+        
     }
 
 
@@ -47,16 +62,34 @@ public class TruckV2 : MonoBehaviour
         
         Currentbreakforce = Input.GetKey(KeyCode.Space) ? breakforce : 0;
         
-        currentDegree = maxDegree * ((Input.GetKey(KeyCode.A) ? 1 : 0) - (Input.GetKey(KeyCode.D) ? 1 : 0));
-        Debug.Log("currentdeggree" + currentDegree);
+        // currentDegree = maxDegree * ((Input.GetKey(KeyCode.A) ? 1 : 0) - (Input.GetKey(KeyCode.D) ? 1 : 0));
+        currentDegree = maxDegree * ((Input.GetKey(KeyCode.D) ? 1 : 0) - (Input.GetKey(KeyCode.A) ? 1 : 0));
         
         foreach (var wCollider in _WheelColliders)
         {
+
+            
             // Accel on all wheels, so its 4 drive
             wCollider.motorTorque = (float)CurrentAccel;
             wCollider.brakeTorque = (float)Currentbreakforce;
             wCollider.steerAngle = (float)currentDegree;
+
+            
+            
+            //TODO FIX THIS CODE :)
+            // wCollider.transform.localScale = Vector3.one;
+            // Vector3 position = default;
+            // Quaternion rotation = default;
+            // wCollider.GetWorldPose(out position, out rotation);
+            // //wCollider.transform.localScale = Vector3.one;
+            // wCollider.transform.position = position;
+            // wCollider.transform.rotation = rotation;
+            
+
+
         }
+        
+        
 
 
 
