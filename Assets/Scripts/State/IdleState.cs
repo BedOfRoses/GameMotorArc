4e9a1e77e16fb2c0a2ad87a892fefdc63bd06ea1
BedOfRoses <50531780+BedOfRoses@users.noreply.github.com>
2023-12-13 00:01:36 +0100

    Update IdleState.cs

--- Assets/Scripts/State/IdleState.cs
+++ Assets/Scripts/State/IdleState.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IdleState : MonoBehaviour
+public class IdleState : IState
 {
-    // Start is called before the first frame update
-    void Start()
+    private readonly TruckV2 _truckV2;
+    public IdleState(TruckV2 truckV2)
     {
-        
+        _truckV2 = truckV2;
+    }
+    
+    public void Enter()
+    {
+        Debug.Log("Entering IdleState");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        // When we are beginning to move
+        // or
+        // When we are braking
+    }
+    
+    public void Exit()
+    {
+        Debug.Log("Exiting IdleState");
     }
 }
