52ff454721706de4040061e374137123e67a6362
BedOfRoses <50531780+BedOfRoses@users.noreply.github.com>
2023-12-13 07:08:40 +0100

    Update TruckV2.cs

--- Assets/Scripts/Vehicle/TruckV2.cs
+++ Assets/Scripts/Vehicle/TruckV2.cs
@@ -6,9 +6,7 @@ using UnityEngine;
 
 public class TruckV2 : MonoBehaviour
 {
-
-    // [SerializeField] private List<GameObject> WheelPrefabs;
-
+    
     #region WheelColliders
     [SerializeField] private WheelCollider frontRightWheelCollider;
     [SerializeField] private WheelCollider frontLeftWheelCollider;
@@ -27,14 +25,19 @@ public class TruckV2 : MonoBehaviour
     [SerializeField] private Rigidbody rb;
 
 
+    #region Experimental Upside down Variables
+    // This was meant to help the player in case they fall upside down
     [SerializeField] private float upsideDownDistance = 3f;
     [SerializeField] private float TimeUpsideDownMax = 3f;
     [SerializeField] private float TimeUpsideDownCurrent = 0f;
+    #endregion
     
-    
+    // Lists for Colliders and Transforms
     [SerializeField] private List<Transform> _WheelTransforms;
     [SerializeField] private List<WheelCollider> _WheelColliders;
 
+    
+    // Setting float variables for acceleration
     [SerializeField] private double accel =  800.300;
     [SerializeField] private double breakforce = 505.405;
     [SerializeField] private double CurrentAccel = default;
@@ -45,7 +48,8 @@ public class TruckV2 : MonoBehaviour
     [SerializeField] private double maxDegree = 37.6;
     [SerializeField] private double turnSpeed = 15;
 
-    private Quaternion startRot;
+    // Reset positions
+    private Transform startRot;
     private Quaternion endRot;
 
     #region StateMachineStuff
@@ -59,8 +63,10 @@ public class TruckV2 : MonoBehaviour
     #endregion
 
 
-    public Camera mainCamera;
 
+    public bool bFunMode = true;
+    public Camera mainCamera;
+    
     // For safety if we tip upside down
     [SerializeField] private Vector3 spawnPos;
     [SerializeField] private Quaternion spawnRot;
@@ -76,11 +82,11 @@ public class TruckV2 : MonoBehaviour
     private void Awake()
     {
         
-        // Set the state
+        // Create and initialize the state
         truckStateMachine = new TruckStateMachine(this);
         truckStateMachine.Initialize(truckStateMachine._idleState); // initialize as idle
 
-
+        // Save the given spawn location of our vehicle as a reset point.
         var transform1 = transform;
         spawnPos = transform1.position;
         spawnRot = transform1.rotation;
@@ -91,19 +97,11 @@ public class TruckV2 : MonoBehaviour
         _WheelColliders.Add(backLeftWheelCollider);
         _WheelColliders.Add(backRightWheelCollider);
         
-        // Transforms
+        // Add Transforms to our list of wheeltransforms
         _WheelTransforms.Add(frontLeftWheelTransform);
         _WheelTransforms.Add(frontRightWheelTransform);
         _WheelTransforms.Add(backLeftWheelTransform);
         _WheelTransforms.Add(backRightWheelTransform);
-        
-       
-        Debug.Log("Transehjul init");
-        foreach (var transehjula in _WheelTransforms)
-        {
-            transehjula.transform.ToString();
-        }
-        
         
     }
 
@@ -111,9 +109,11 @@ public class TruckV2 : MonoBehaviour
     private void FixedUpdate()
     {
 
+        // Update camera to look at our vehicle
         if (mainCamera)
             mainCamera.transform.LookAt(transform.position);
         
+        // State machine update and text update to let us know what state we're in!
         truckStateMachine.Update();
         stateText.text = truckStateMachine.CurrentState.ToString();
 
@@ -150,15 +150,15 @@ public class TruckV2 : MonoBehaviour
         Currentbreakforce = Input.GetKey(KeyCode.Space) ? breakforce : 0; // either breakforce or 0
         
         // turn left and right
-        currentDegree = maxDegree * ((Input.GetKey(KeyCode.D) ? 1 : 0) - (Input.GetKey(KeyCode.A) ? 1 : 0)); //gives either 1, 0 or -1
+        currentDegree = maxDegree * ((Input.GetKey(KeyCode.D) ? 1 : 0) - (Input.GetKey(KeyCode.A) ? 1 : 0)); //gives either 1, 0 or -1 times the amount of degrees. 
         
-        double rotationAmount = maxDegree * currentDegree / maxDegree;
+        // double rotationAmount = maxDegree * currentDegree / maxDegree; // Was used for debugging and testing/experimenting
         
         foreach (var wCollider in _WheelColliders)
         {
             // Accel on all wheels, so its bascially 4-drive
-            wCollider.motorTorque = (float)CurrentAccel;
-            wCollider.brakeTorque = (float)Currentbreakforce;
+            wCollider.motorTorque = (float)CurrentAccel; // The torque applied to rotate wheel in the given directions
+            wCollider.brakeTorque = (float)Currentbreakforce; // The force to slow the wheels rotation
             // wCollider.steerAngle = (float)currentDegree; // This sets every wheel to turn, basically we just steer sideways. No turning with this on
         }
 
@@ -167,12 +167,26 @@ public class TruckV2 : MonoBehaviour
         frontRightWheelCollider.steerAngle = (float)currentDegree;
         
         // Update transforms
-        WheelRotUpdate(frontLeftWheelCollider,frontLeftWheelTransform);
-        WheelRotUpdate(frontRightWheelCollider,frontRightWheelTransform);
-        WheelRotUpdate(backLeftWheelCollider,backLeftWheelTransform);
-        WheelRotUpdate(backRightWheelCollider,backRightWheelTransform);
+        if (bFunMode)
+        {
+            WheelRotUpdate(frontLeftWheelCollider,frontLeftWheelTransform);
+            WheelRotUpdate(frontRightWheelCollider,frontRightWheelTransform);
+            WheelRotUpdate(backLeftWheelCollider,backLeftWheelTransform);
+            WheelRotUpdate(backRightWheelCollider,backRightWheelTransform);
+        }
+        else
+        {
+            for (int i = 0; i < _WheelTransforms.Count; i++)
+            {
+                _WheelTransforms[i].localRotation = Quaternion.Euler(
+                    _WheelTransforms[i].localEulerAngles.x,
+                    _WheelColliders[i].steerAngle,
+                    _WheelTransforms[i].localEulerAngles.z);
+            }
+        }
         
-
+        
+        
         // Statemachine info stuff
         IsMoving = rb.velocity.magnitude >= 0.02f;
         IsBreaking = Currentbreakforce > 0;
@@ -191,10 +205,14 @@ public class TruckV2 : MonoBehaviour
 
     public void WheelRotUpdate(WheelCollider wColl, Transform tf)
     {
+        // Variables for rotation and position
         Vector3 pos;
         Quaternion rot;
+        
+        // Extract world position and rotation from collider
         wColl.GetWorldPose(out pos, out rot);
 
+        // Update the position and rotation the same as collider.
         tf.position = pos;
         tf.rotation = rot;
     }
